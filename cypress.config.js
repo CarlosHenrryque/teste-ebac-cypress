dd66b9d266d6bb728eda632b32622a1b2ff8062b
Carlos Henrryque <kaike.ch03@gmail.com>
2024-01-03 11:58:19 -0300

    Usando massa de dados

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '9r5sde',
+
   e2e: {
+
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+
+
+
+
+
+
     },
+
   },
+
 });
