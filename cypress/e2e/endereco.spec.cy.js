64dc6c478fe1e9b82d98d2ee90dd9776781e0efb
Carlos Henrryque <kaike.ch03@gmail.com>
2024-01-05 14:49:13 -0300

    Cadastro Usando massa  de dados

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Fucnionaldiade Endereços - Faturamento e Entrego', () => {
     beforeEach(() => {
@@ -10,9 +11,27 @@ describe('Fucnionaldiade Endereços - Faturamento e Entrego', () => {
 
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         enderecoPage.editarEnderecoFaturamento('Carlos', 'Sousa', 'PD', 'Planaltina-DF', 15, 'Brasília', 'Distrito Federal', 73402099, 11993869060, 'kaikeebac@gmail.com')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            ///dadosEndereco[0].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email,
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
 
 });
